ce539fca6f65fb576e3e2a25e0ce7879791b6afd
ncarraz <carraznathanael@live.fr>
2022-03-03 11:35:30 +0300

    Update run_experiments.py

--- scripts/run_experiments.py
+++ scripts/run_experiments.py
@@ -101,7 +101,7 @@ def run_experiments(
     type_count = defaultdict(list)
 
     results_file = open("last_results.csv", "w+")
-    output_file = open("output_results_{}.csv".format(my_args.relations), "a")
+    output_file = open("output_results_{}.csv".format(os.path.basename(my_args.relations)), "a")
 
     for relation in relations:
         pp.pprint(relation)
